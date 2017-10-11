e4e1511b0131990382b58dbaa217347c4c0fb2f4
Sikorskiy Dmitriy <sikorskiy.dima@yandex.ru>
2017-10-11 22:29:48 +0300

    Add files via upload

--- portfolio/Plomin.dev/js/main.js
+++ portfolio/Plomin.dev/js/main.js
@@ -1,1 +1,141 @@
-"use strict";$(function(e){e(function(){var o=e("#headerNav"),t=e("#openNav");o.on("click","a",function(o){o.preventDefault();var t=e(this).attr("href"),a=e(t).offset().top;e("body,html").animate({scrollTop:a},1500)}),t.on("click",function(t){t.preventDefault(),e(this).toggleClass("active"),o.toggleClass("visible"),e("body").toggleClass("hidden")}),e(".header-slider").slick({dots:!0,arrows:!1,autoplay:!0,infinite:!0,slidesToShow:1,slidesToScroll:1,lazyLoad:"ondemand",autoplaySpeed:3e4}),e("#eventsSlider").slick({dots:!0,arrows:!1,autoplay:!0,infinite:!0,slidesToShow:1,slidesToScroll:1,lazyLoad:"ondemand",autoplaySpeed:3e4,dotsClass:"events-dots slick-dots"}),e("#mainGalleryTabs").tabs({active:1}),e(".main_gallery-video").slick({dots:!1,arrows:!0,autoplay:!0,infinite:!0,centerMode:!0,slidesToScroll:1,variableWidth:!0,lazyLoad:"ondemand",responsive:[{breakpoint:1920,settings:{slidesToShow:3}},{breakpoint:768,settings:{slidesToShow:1}}],prevArrow:'<button type="button" class="main_gallery-arrow main_gallery-prev"></button>',nextArrow:'<button type="button" class="main_gallery-arrow main_gallery-next"></button>'})})}(jQuery));
+'use strict';
+
+(function(){
+	
+	
+	
+}());
+
+
+$(function($){
+	$(function(){
+
+		
+		var nav = $('#headerNav');
+		var openNav = $('#openNav');
+		
+		nav.on("click", "a", function (e) {
+			e.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+		
+		
+		openNav.on('click', function(e){
+			e.preventDefault();
+			$(this).toggleClass('active')
+			nav.toggleClass('visible');
+			$('body').toggleClass('hidden');
+		});
+
+		$('.header-slider').slick({
+			dots: true,
+			arrows: false,
+			//autoplay: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			lazyLoad: 'ondemand',
+			autoplaySpeed: 30000,
+		});
+
+		$('#eventsSlider').slick({
+			dots: true,
+			arrows: false,
+			autoplay: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			lazyLoad: 'ondemand',
+			autoplaySpeed: 30000,
+			dotsClass: 'events-dots slick-dots',
+		});
+
+		 $("#mainGalleryTabs").tabs({
+			active: 0,
+		 });
+
+		$('.main_gallery-video').slick({
+			dots: false,
+			arrows: true,
+			autoplay: true,
+			infinite: true,
+			centerMode: true,
+			slidesToScroll: 1,
+			//useTransform: true,
+			variableWidth: true,
+			lazyLoad: 'ondemand',
+			responsive: [
+				{
+					breakpoint: 1920,
+					settings: {
+						slidesToShow: 3,
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+					}
+				},
+			],
+			prevArrow: '<button type="button" class="main_gallery-arrow main_gallery-prev"></button>',
+			nextArrow: '<button type="button" class="main_gallery-arrow main_gallery-next"></button>',
+		});
+
+		function PhotoGallery(e){
+			var container = $('#mainImagesGallery');
+			var containerBlur = $('.main_images-gallery.blur');
+			var content = $('.main_images-content');
+			var contentActive = $('.main_images-content.active');
+			var fullSizeLink = $('.main_images-expand');
+			var closeBtn = $('.main_images-close');
+			
+			container.find('a').on('click', function(e){
+				e.preventDefault();
+			});
+			
+			container.find(content).each(function(i){
+				$(this).attr('id', 'image-' + ++i);
+			});
+			
+			container.find(fullSizeLink).on('click', function(e){
+				content.removeClass('active');
+				$(this).parent(content).addClass('active');
+				container.addClass('blur');
+			});
+
+			closeBtn.on('click', function(e){
+				$(this).parent(content).removeClass('active');
+				container.removeClass('blur');
+			});
+			
+			$(document).mouseup(function (e){ // событие клика по веб-документу
+				if (!content.is(e.target) // если клик был не по нашему блоку
+					&& content.has(e.target).length === 0) { // и не по его дочерним элементам
+					content.removeClass('active');
+					container.removeClass('blur');
+				}
+			});
+			
+			// вызов функции при ресайзе окна
+			$(window).on('resize load', function() {
+				// функция центрирования элемента
+				function alignCenter() {
+					contentActive.css({
+						// вычисление координат left и top
+						left: ($(window).width() - contentActive.width()) / 2 + 'px',
+						top: ($(window).height() - contentActive.height()) / 2 + 'px'
+					});
+				}
+				
+				alignCenter(contentActive);	
+			});
+		}
+		
+		PhotoGallery();
+		
+	});
+	
+}(jQuery));
